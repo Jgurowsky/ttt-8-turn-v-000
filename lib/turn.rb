d53cf62aa11993cb8d8bfdd17f95a9dad7a84dcb
jgurowsky@gmail.com <jgurowsky@gmail.com>
2018-05-25 17:18:24 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(index)
+def input_to_index(user_input)
   user_input.to_i - 1
 end
 
@@ -31,6 +31,6 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
-  input_to_index(index)
+  input_to_index(user_input)
 
 end
